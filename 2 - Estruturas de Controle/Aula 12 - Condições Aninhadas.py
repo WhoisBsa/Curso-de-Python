d812ae999bb86258a52fedde1a81140d970a017d
WhoisBsa <mbscbsjbs@gmail.com>
2018-11-20 14:43:47 -0300

    Aula 12

--- 2 - Estruturas de Controle/Aula 12 - Condições Aninhadas.py
+++ 2 - Estruturas de Controle/Aula 12 - Condições Aninhadas.py
@@ -3,6 +3,8 @@ if nome == 'Matheus':
     print('Que nome bonito!')
 elif nome == 'João' or nome == 'Maria' or nome == 'José':
     print('Seu nome é bem popular no Brasil.')
+elif nome in ('Ana Claudia Juliana'):
+    print('Belo nome femininno')
 else:
     print('Seu nome é normal.')
 print('Tenha um bom dia, {}'.format(nome))
