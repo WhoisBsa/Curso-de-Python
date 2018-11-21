17009d63fd78546df44c9baec71814904267e88b
WhoisBsa <mbscbsjbs@gmail.com>
2018-11-21 14:50:07 -0300

    Desafio 44

--- 2 - Estruturas de Controle/desafio 45, jokenpô.py
+++ 2 - Estruturas de Controle/desafio 45, jokenpô.py
@@ -10,22 +10,31 @@ condicao = True
 while condicao:
     escolhadopc = choice(PPT)
     suaescolha = str(input('Escolha (Pedra, Papel ou Tesoura): '))
+
     if suaescolha in PPT:
         print('Sua escolha foi {} e a do computador foi {}, o resultado foi que '.format(suaescolha, escolhadopc), end='')
+
         if escolhadopc == suaescolha:
             print('houve empate')
+
         elif escolhadopc == 'Pedra' and suaescolha == 'Tesoura':
             print('o computador venceu')
+
         elif escolhadopc == 'Tesoura' and suaescolha == 'Papel':
             print('o computador venceu')
-        elif escolhadopc == 'Tesoura' and suaescolha == 'Pedra':
-            print('você venceu')
-        elif escolhadopc == 'Papel' and suaescolha == 'Tesoura':
-            print('você venceu')
-        elif escolhadopc == 'Pedra' and suaescolha == 'Papel':
-            print('você venceu')
+
         elif escolhadopc == 'Papel' and suaescolha == 'Pedra':
             print('o computador venceu')
+
+        elif escolhadopc == 'Tesoura' and suaescolha == 'Pedra':
+            print('você venceu')
+
+        elif escolhadopc == 'Papel' and suaescolha == 'Tesoura':
+            print('você venceu')
+
+        elif escolhadopc == 'Pedra' and suaescolha == 'Papel':
+            print('você venceu')
+
         else:
             print('essa não é uma opção válida')
     else:
