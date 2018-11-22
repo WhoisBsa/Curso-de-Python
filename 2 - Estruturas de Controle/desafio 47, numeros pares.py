6d7633d90545a385b5e9aeddbfb1f205f757c3e3
WhoisBsa <mbscbsjbs@gmail.com>
2018-11-22 14:33:10 -0300

    Revert "Desafio 44"
    This reverts commit 61007ea503883cc3146c0324ef902855019ea718.

--- 2 - Estruturas de Controle/desafio 47, numeros pares.py
+++
@@ -1,7 +0,0 @@
-"""
-Crie um programa que mostre na tela todos os números pares que estão
-no intervalo entre 1 e 50.
-"""
-for i in range(0, 51):
-    if i % 2 == 0:
-        print(i, end=' ')
--- 2 - Estruturas de Controle/desafio 48, soma numeros impares.py
+++
@@ -1,9 +0,0 @@
-"""
-Faça um programa que calcule a soma entre todos os números ímpares que são
-múltiplos de três e que se encontram no intervalo de 1 até 500
-"""
-soma = 0
-for i in range(0, 501):
-    if i % 3 == 0 and i % 2 != 0:
-        soma += i
-print(soma, end=' ')
