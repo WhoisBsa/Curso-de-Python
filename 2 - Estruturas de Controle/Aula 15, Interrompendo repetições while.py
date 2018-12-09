23a4c6b0a27111a2f3204e75896a12a95fb4c986
WhoisBsa <mbscbsjbs@gmail.com>
2018-12-09 15:46:38 -0300

    Aula 15

---
+++ 2 - Estruturas de Controle/Aula 15, Interrompendo repetições while.py
@@ -0,0 +1,25 @@
+'''cont = 1
+while cont <=10:
+    print(cont, end=' ')
+    cont += 1
+print('Acabou')'''
+
+'''cont = 1
+while True:
+    print(cont, end=' ')
+    cont += 1
+print('Acabou')'''
+
+'''n = s = 0
+while n != 999:
+    n = int(input('Digite um número: '))
+    s += n
+print('A soma vale {}'.format(s))'''
+
+'''n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print('A soma vale {}'.format(s))'''
