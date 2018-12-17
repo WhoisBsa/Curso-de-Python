e0daabb21b4212db37ffa4572089c78fa28c5410
WhoisBsa <mbscbsjbs@gmail.com>
2018-12-17 16:43:42 -0300

    Aula 16

---
+++ 3 - Estruturas Compostas/Aula 16 - Tuplas.py
@@ -0,0 +1,21 @@
+lanche = (git'Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche[-1])
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche[1:3])
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche)
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+for c in lanche:
+	print(f'Eu vou comer {c}')
+print('Comi pra caramba!')
+
+for c in range(0, len(lanche)):
+	print(f'Eu vou comer {lanche[c]}')
+print('Comi pra caramba!')
+
+for pos, c in enumerate(lanche):
+	print(f'Eu vou comer {c} na posicao {pos}')
+print('Comi pra caramba!')
