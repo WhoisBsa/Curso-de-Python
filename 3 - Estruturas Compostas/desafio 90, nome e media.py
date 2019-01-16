bb028eddef8ccbbebaa84a8aa58844ba2a423868
WhoisBsa <mbscbsjbs@gmail.com>
2019-01-16 08:25:15 -0300

    desafios

---
+++ 3 - Estruturas Compostas/desafio 90, nome e media.py
@@ -0,0 +1,15 @@
+"""
+Faça um programa que leia nome e média de um aluno,
+guardando também a situação em um dicionário. No final,
+mostre o conteúdo da estrutura na tela.
+"""
+
+aluno = dict()
+
+aluno['nome'] = str(input('Nome: '))
+aluno['nota'] = int(input(f'Média de {aluno["nome"]}: '))
+media = aluno['nota']
+aluno['situacao'] = 'Aprovado' if media >= 7 else 'Reprovado'
+
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}')
