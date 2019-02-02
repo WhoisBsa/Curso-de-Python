b247833ee02f784d597f2dad15de57eff4c1797e
WhoisBsa <mbscbsjbs@gmail.com>
2019-02-02 09:59:58 -0200

    Aula 12

--- 3 - Estruturas Compostas/Aula 20 - Funcoes.py
+++ 3 - Estruturas Compostas/Aula 20 - Funcoes.py
@@ -27,3 +27,36 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def contador(*num):
+	tam = len(num)
+print(f'Recebi os valores {num} e sao ao todo {tam} numeros')
+
+
+contador(2, 4, 5, 8)
+contador(1, 9, 3)
+contador(3, 2)
+
+
+def dobra(lst):
+	pos = 0
+	while pos < len(lst):
+		lst[pos] *= 2
+		pos += 1
+
+
+valores = [6, 7, 2, 5, 4, 0, 1]
+dobra(valores)
+print(valores)
+
+
+def soma(*valores):
+	s = 0
+	for num in valores:
+		s += num
+	print(f'Somando os valores {valores} temos {s}')
+
+
+soma(5, 2)
+soma(2, 9, 4)
